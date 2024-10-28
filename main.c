324602395e5029f944f7ced5607bf5b4e2721c4c
JustWSJ <167499209+JustWSJ@users.noreply.github.com>
2024-10-28 12:50:02 +0900

    Update main.c input any number&print

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main(){
+  //  random num scan&print
   printf("Open source SW project\n");
   pritnf("First GitHub Training\n");
+  int num;
+  printf("input any number >> ");
+  scnaf("%d", &num);
+  printf("input number is : %d", num);
   return 0;
 }
